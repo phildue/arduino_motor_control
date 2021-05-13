d629dc864affab9438b15a318af63de5d7d9cc90
phil <phild@protonmail.com>
2021-05-13 15:39:12 +0200

    added set point and control gain setting via uart, some cleanup

---
+++ SystemArduino.h
@@ -0,0 +1,61 @@
+#include <System.h>
+class SystemArduino : public robopi::System
+{
+public:
+    void sleep(int msec) override{ delay(msec);};
+
+    void setMode(int pin,int sig) override{
+        switch(sig)
+        {
+            case PI_OUTPUT:
+                pinMode(pin,OUTPUT);
+                break;
+            case PI_INPUT:
+                pinMode(pin,INPUT);
+                break;
+            default:
+                Serial.println("Wrong pin setup.");
+        }
+    };
+
+    void write(int pin,int sig) override{
+        switch (sig)
+        {
+            case PI_ON:
+                digitalWrite(pin,HIGH);
+                break;
+            case PI_OFF:
+                digitalWrite(pin,LOW);
+                break;
+            default:
+                Serial.println("Wrong pin write.");
+        }
+    };
+    void pWM(int pin,int sig) override{
+        
+        analogWrite(pin,sig);
+    };
+    int read(int pin) override{
+        switch (digitalRead(pin))
+        {
+            case HIGH:
+                return PI_ON;
+            case LOW:
+                return PI_OFF;
+            default:
+                return PI_OFF;
+        }
+    };
+
+    void setPWMfrequency(int pin,int freq) override{};
+    void setPullUpDown(int pin,int upDown) override{
+        switch (upDown)
+        {
+            case PI_PUD_UP:
+                pinMode(pin,INPUT_PULLUP);
+                break;
+            default:
+                Serial.println("Wrong pin setup.");
+        }
+    };
+};
--- motor_control.ino
+++ motor_control.ino
@@ -2,7 +2,7 @@
 #include <MotorLn298.h>
 #include <MotorVelocityControl.h>
 #include <SonarHcsr04.h>
-
+#include "SystemArduino.h"
 #ifdef ARDUINO_ARCH_MEGAAVR
 #include "EveryTimerB.h"
 #define Timer1 TimerB2 // use TimerB2 as a drop in replacement for Timer1
@@ -13,168 +13,272 @@
 using namespace robopi;
 
 /// Pin Layout
-constexpr int in1 = 2,in2 = 3,enA = 9;
-constexpr int in3 = 4,in4 = 5,enB = 10;
-constexpr int encRight = 6,encLeft = 7;
+constexpr int in1 = 2, in2 = 3, enA = 9;
+constexpr int in3 = 4, in4 = 5, enB = 10;
+constexpr int encRight = 6, encLeft = 7;
 
 /// Control Parameters
-constexpr double kp = 0.02,ki = 0.05, kd = 0.00;
-constexpr double f = 200;//[hz]
-constexpr double dT = 1.0/f;//[s]
-constexpr double kpObs = 2.5, kiObs = 7.5;
-constexpr double setPoint = 10;//[rad/s]
+constexpr double kp = 0.02, ki = 0.05, kd = 0.00;
+volatile double kpR = kp, kiR = ki, kdR = kd;
+volatile double kpL = kp, kiL = ki, kdL = kd;
+
+constexpr double f = 1000;//[hz]
+constexpr double dT = 1.0 / f; //[s]
+constexpr double kpObs = 3.0, kiObs = 7.5;
+double setPointLeft = 0;//[rad/s]
+double setPointRight = 0;//[rad/s]
 
 constexpr double S_TO_MS = 1000.0;
 constexpr double S_TO_US = 1000.0 * 1000.0;
+constexpr double V_MAX = 15.0;
 
+volatile System* gpios;
+volatile Encoder* encoderLeft;
+volatile Encoder* encoderRight;
+volatile MotorLn298* motorLeft;
+volatile MotorLn298* motorRight;
+volatile VelocityEstimator* filterLeft;
+volatile VelocityEstimator* filterRight;
+volatile MotorVelocityControl* controlLeft;
+volatile MotorVelocityControl* controlRight;
+volatile bool running = true;
 
-class SystemArduino : public System
-{
-public:
-    void sleep(int msec) override{ delay(msec);};
-
-    void setMode(int pin,int sig) override{
-        switch(sig)
-        {
-            case PI_OUTPUT:
-                pinMode(pin,OUTPUT);
-                break;
-            case PI_INPUT:
-                pinMode(pin,INPUT);
-                break;
-            default:
-                Serial.println("Wrong pin setup.");
-        }
-    };
-
-    void write(int pin,int sig) override{
-        switch (sig)
-        {
-            case PI_ON:
-                digitalWrite(pin,HIGH);
-                break;
-            case PI_OFF:
-                digitalWrite(pin,LOW);
-                break;
-            default:
-                Serial.println("Wrong pin write.");
-        }
-    };
-    void pWM(int pin,int sig) override{
-        
-        analogWrite(pin,sig);
-    };
-    int read(int pin) override{
-        switch (digitalRead(pin))
-        {
-            case HIGH:
-                return PI_ON;
-            case LOW:
-                return PI_OFF;
-            default:
-                return PI_OFF;
-        }
-    };
-
-    void setPWMfrequency(int pin,int freq) override{};
-    void setPullUpDown(int pin,int upDown) override{
-        switch (upDown)
-        {
-            case PI_PUD_UP:
-                pinMode(pin,INPUT_PULLUP);
-                break;
-            default:
-                Serial.println("Wrong pin setup.");
-        }
-    };
-};
-
-System* gpios;
-Encoder* encoderLeft;
-Encoder* encoderRight;
-MotorLn298* motorLeft;
-MotorLn298* motorRight;
-VelocityEstimator* filterLeft;
-VelocityEstimator* filterRight;
-MotorVelocityControl* controlLeft;
-MotorVelocityControl* controlRight;
 unsigned long tick = 0U;
-constexpr unsigned int deltaTick = static_cast<unsigned long>(0.1*S_TO_MS);
+unsigned long lastCommand = 0U;
+unsigned long commandTimeout = 10U;
+constexpr unsigned int deltaTick = static_cast<unsigned long>(0.1 * S_TO_MS);
 void isr_encRight()
 {
-    encoderRight->tick();
+  encoderRight->tick();
 }
 void isr_encLeft()
 {
-    encoderLeft->tick();
+  encoderLeft->tick();
 }
 
-void isr_update(){
+void isr_update() {
+  if (running)
+  {
     controlRight->update(dT);
     controlLeft->update(dT);
+  }
 }
+
+
+
+
 void setup()
 {
-    Serial.begin(9600);
+  Serial.begin(9600);
 
-    gpios = new SystemArduino();
-    motorRight = new MotorLn298(in1,in2,enA,gpios);
-    encoderRight = new Encoder(encRight,gpios);
+  gpios = new SystemArduino();
+  motorRight = new MotorLn298(in1, in2, enA, gpios);
+  encoderRight = new Encoder(encRight, gpios);
 
-    //auto filterRight = std::make_shared<SlidingAverageFilter>(filterSize);
-    filterRight = new LuenbergerObserver(kpObs,kiObs);
+  //auto filterRight = std::make_shared<SlidingAverageFilter>(filterSize);
+  filterRight = new LuenbergerObserver(kpObs, kiObs);
 
-    controlRight = new MotorVelocityControl(motorRight,encoderRight,filterRight,kp,ki,kd);
+  controlRight = new MotorVelocityControl(motorRight, encoderRight, filterRight, kp, ki, kd);
 
-    motorLeft = new MotorLn298(in4,in3,enB,gpios);
-    encoderLeft = new Encoder(encLeft,gpios);
+  motorLeft = new MotorLn298(in4, in3, enB, gpios);
+  encoderLeft = new Encoder(encLeft, gpios);
 
-    attachInterrupt(digitalPinToInterrupt(encRight), isr_encRight, CHANGE);
-    attachInterrupt(digitalPinToInterrupt(encLeft), isr_encLeft, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(encRight), isr_encRight, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(encLeft), isr_encLeft, CHANGE);
 
-    //auto filterLeft = std::make_shared<SlidingAverageFilter>(filterSize);
-    filterLeft = new LuenbergerObserver(kpObs,kiObs);
+  //auto filterLeft = std::make_shared<SlidingAverageFilter>(filterSize);
+  filterLeft = new LuenbergerObserver(kpObs, kiObs);
 
-    controlLeft = new MotorVelocityControl(motorLeft,encoderLeft,filterLeft,kp,ki,kd);
+  controlLeft = new MotorVelocityControl(motorLeft, encoderLeft, filterLeft, kp, ki, kd);
 
-    Timer1.initialize();
-    Timer1.attachInterrupt(isr_update);
-    Timer1.setPeriod(dT*S_TO_US); 
-      
+  Timer1.initialize();
+  Timer1.attachInterrupt(isr_update);
+  Timer1.setPeriod(dT * S_TO_US);
+
 
 }
 
+void readSerial();
+void writeSerial();
 
-
-void plotStatus(const MotorVelocityControl* controller)
+void updateState()
 {
-      //Serial.print(controller->wheelTicks());
-      //Serial.print(" ");
-      Serial.print(100);
-      Serial.print(" ");
-      Serial.print((controller->velocity()/controller->velocitySet())*100);
-      Serial.print(" ");
-      //Serial.print(controller->error());
-      //Serial.print(" ");
-      Serial.print(controller->dutySet());
+   if (setPointRight > V_MAX)
+   {
+    setPointRight = V_MAX;
+   }
+   if (setPointRight < -1.0 * V_MAX)
+   {
+    setPointRight = -1.0 * V_MAX;
+   }
+   if(setPointLeft > V_MAX)
+   {
+    setPointLeft = V_MAX;
+   }
+   if (setPointLeft < -1.0 * V_MAX)
+   {
+    setPointLeft = -1.0 * V_MAX;
+   }
+   controlRight->set(setPointRight);
+   controlLeft->set(setPointLeft);
+   if (setPointRight > 0 || setPointLeft > 0)
+   {
+    running = true;
+   }
+   if (tick - lastCommand > commandTimeout * S_TO_MS)
+   {
+    lastCommand = tick;
+    Serial.println("Timeout!");
+    setPointLeft = 0.0;
+    setPointRight = 0.0;
+    running = false;
+    motorLeft->stop();
+    motorRight->stop();
+    }
+
 }
+
 
 void loop()
 {
-    controlRight->set(setPoint);
-    controlLeft->set(setPoint);
 
-   
+  //setPoint = sin((float)tick/10000.0)*20.0;
+
+  readSerial();
+
+  updateState();
+ 
+  writeSerial();
+
+
+  delay(deltaTick);
+  tick += deltaTick;
+
+}
+
+void handleSerial()
+{
+  if (Serial.available() > 0)
+  {
+    String msg = Serial.readStringUntil("\n");
+    Serial.println("I recieved:");
+    Serial.println(msg);
+    lastCommand = tick;
+    if (msg.startsWith("vl"))
+    {
+      setPointLeft = msg.substring(3).toFloat();
      
-      plotStatus(controlLeft);
-            Serial.print(" ");
+      Serial.println("Set point left:");
+      Serial.println(setPointLeft);
 
-      plotStatus(controlRight); 
-            Serial.print("\r\n");
-   
+    } else if (msg.startsWith("vr"))
+    {
+      setPointRight = msg.substring(3).toFloat();
+      Serial.println("Set point right:");
+      Serial.println(setPointRight);
 
-    
-    delay(deltaTick);
-    tick += deltaTick;
+    }
+    else if (msg.startsWith("kpr"))
+    {
+      kpR = msg.substring(4).toFloat();
+      Serial.println("Setting kpR:");
+      Serial.println(kpR);
+
+    }
+    else if (msg.startsWith("kir"))
+    {
+      kiR = msg.substring(4).toFloat();
+      Serial.println("Setting kiR:");
+      Serial.println(kiR);
+
+    }
+    else if (msg.startsWith("kdr"))
+    {
+      kdR = msg.substring(4).toFloat();
+      Serial.println("Setting kdR:");
+      Serial.println(kdR);
+    }
+    else if (msg.startsWith("kpl"))
+    {
+      kpL = msg.substring(4).toFloat();
+      Serial.println("Setting kpL:");
+      Serial.println(kpL);
+
+    }
+    else if (msg.startsWith("kil"))
+    {
+      kiL = msg.substring(4).toFloat();
+      Serial.println("Setting kiL:");
+      Serial.println(kiL);
+
+    }
+    else if (msg.startsWith("kdl"))
+    {
+      kdL = msg.substring(4).toFloat();
+      Serial.println("Setting kdL:");
+      Serial.println(kdL);
+
+    }
+    else if (msg.startsWith("kp"))
+    {
+      kpR = msg.substring(4).toFloat();
+      kpL = kpR;
+      Serial.println("Setting kp:");
+      Serial.println(kpR);
+
+    }
+    else if (msg.startsWith("ki"))
+    {
+      kiR = msg.substring(4).toFloat();
+      kiL = kiR;
+      Serial.println("Setting ki:");
+      Serial.println(kiR);
+
+    }
+    else if (msg.startsWith("kd"))
+    {
+      kdR = msg.substring(4).toFloat();
+      kdL = kdR;
+      Serial.println("Setting kd:");
+      Serial.println(kdR);
+
+    } else if (msg.startsWith("p"))
+    {
+      Serial.println(String(" kpR = " ) + String(kpR) + String(" kiR = ") + String(kiR) + String(" kdR = ") + String(kdR));
+      Serial.println(String(" kpL = " ) + String(kpL) + String(" kiL = ") + String(kiL) + String(" kdL = ") + String(kdL));
+    }
+    else {
+      Serial.println("Unknown Command");
+      setPointLeft = 0;
+      setPointRight = 0;
+      running = false;
+      motorLeft->stop();
+      motorRight->stop();
+    }
+  }
+}
+
+void plotStatus(const MotorVelocityControl* controller)
+{
+  //Serial.print(controller->wheelTicks());
+  //Serial.print(" ");
+  Serial.print(controller->velocitySet());
+  Serial.print(" ");
+  Serial.print((controller->velocity()));
+  //Serial.print(" ");
+  //Serial.print(controller->error());
+  //Serial.print(" ");
+  //Serial.print(controller->dutySet());
+}
+
+void plotStatus()
+{
+
+  plotStatus(controlLeft);
+  Serial.print(" ");
+
+  plotStatus(controlRight);
+  Serial.print("\r\n");
+
 
 }
